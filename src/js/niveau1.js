d33b963fd6ee116e082acfa30a95ed187b35f594
Beatrix Eysseric <beatrix.eysseric@epfedu.fr>
2025-03-11 12:50:24 +0100

    midi

--- src/js/niveau1.js
+++ src/js/niveau1.js
@@ -8,60 +8,62 @@ export default class niveau1 extends Phaser.Scene {
     });
   }
   preload() {
-    // chargement tuiles de jeu
-    this.load.image("Phaser_Tuile_de_jeuV0", "src/assets/pixil-frame-0 (1).png");
-
-    // chargement de la carte
-    this.load.tilemapTiledJSON("carte", "src/assets/map.json");
-    this.load.image("img_porte1", "src/assets/porte1.png");
-    this.load.image("img_porte2", "src/assets/porte2.png");
-    this.load.image("img_perso", "src/assets/perso.png");
-    this.gameOver = false;
   }
 
   create() {
-    // chargement de la carte
-    const carteDuNiveau = this.add.tilemap("carte");
-
-    // chargement du jeu de tuiles
-    const tileset = carteDuNiveau.addTilesetImage(
-          "Tuile_de_jeuV0",
-          "Phaser_Tuile_de_jeuV0"
-        ); 
-    // chargement du calque calque_background
-    const calque_background = carteDuNiveau.createLayer(
-          "Calque de Tuiles 1",
-          tileset
-        );
-
-    // chargement du calque calque_background_2
-    const calque_background_2 = carteDuNiveau.createLayer(
-        "route",
-        tileset
-      );
-    calque_background_2.setCollisionByProperty({ estSolide: true }); 
-
-    // redimentionnement du monde avec les dimensions calculées via tiled
-    this.physics.world.setBounds(0, 0, 3200, 640);
-    //  ajout du champs de la caméra de taille identique à celle du monde
-    this.cameras.main.setBounds(0, 0, 3200, 640);
-    // ancrage de la caméra sur le joueur
+    this.add.image(400, 300, "img_maison_b");
+    this.groupe_plateformes = this.physics.add.staticGroup();
+    this.groupe_plateformes.create(200, 584, "img_plateforme_b");
+    this.groupe_plateformes.create(600, 584, "img_plateforme_b");
     
-    this.porte_retour = this.physics.add.staticSprite(200, 450, "img_porte1");
-    this.porte2 = this.physics.add.staticSprite(750, 450, "img_porte2");
+    
+    this.groupe_plateformes.create(140, 460, "img_plateforme_be");
+    this.groupe_plateformes.create(640, 460, "img_plateforme_be");
+    this.groupe_plateformes.create(0, 370, "img_plateforme_be");
+    this.groupe_plateformes.create(165, 280, "img_plateforme_be");
+    this.groupe_plateformes.create(625, 280, "img_plateforme_be");
+    this.groupe_plateformes.create(0, 280, "img_plateforme_be");
+    this.groupe_plateformes.create(700, 280, "img_plateforme_be");
+    this.groupe_plateformes.create(140, 170, "img_plateforme_be");
+    this.groupe_plateformes.create(300, 170, "img_plateforme_be");
+    this.groupe_plateformes.create(500, 170, "img_plateforme_be");
+    this.groupe_plateformes.create(655, 170, "img_plateforme_be");
+
+    this.groupe_plateformes.create(450, 500, "img_plateforme_mini");
+    this.groupe_plateformes.create(450, 505, "img_plateforme_mini");
+    this.groupe_plateformes.create(340, 500, "img_plateforme_mini");
+    this.groupe_plateformes.create(340, 505, "img_plateforme_mini");
+    this.groupe_plateformes.create(395, 350, "img_plateforme_mini");
+    this.groupe_plateformes.create(395, 355, "img_plateforme_mini"); 
+    this.groupe_plateformes.create(395, 130, "img_plateforme_mini");
+    this.groupe_plateformes.create(395, 125, "img_plateforme_mini");
+    this.groupe_plateformes.create(370, 130, "img_plateforme_mini");
+    this.groupe_plateformes.create(370, 125, "img_plateforme_mini");
+    this.groupe_plateformes.create(420, 130, "img_plateforme_mini");
+    this.groupe_plateformes.create(420, 125, "img_plateforme_mini");
+
+
+
+
+
+
+    // ajout d'un texte distintcif  du niveau
+    this.add.text(420, 50, "Vous êtes dans le niveau 1", {
+      fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
+      fontSize: "22pt",
+      color: "#000000" // Définit la couleur du texte en noir
+    });
+
+    this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte1");
+    this.porte2 = this.physics.add.staticSprite(397, 100, "img_porte2");
+
     this.player = this.physics.add.sprite(100, 450, "img_perso");
     this.player.refreshBody();
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
-    this.physics.add.collider(this.player, calque_background_2);
-    this.cameras.main.startFollow(this.player);
     this.clavier = this.input.keyboard.createCursorKeys();
-    this.startCountdown(); 
     this.physics.add.collider(this.player, this.groupe_plateformes);
-    this.startCountdown(); //démare le compte à rebours
   }
-
-  
 
   update() {
     if (this.clavier.left.isDown) {
@@ -74,10 +76,10 @@ export default class niveau1 extends Phaser.Scene {
       this.player.setVelocityX(0);
       this.player.anims.play("anim_face");
     }
-    if (this.clavier.up.isDown && this.player.body.blocked.down) {
-      this.player.setVelocityY(-200);
+    if (this.clavier.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
     }
-    
+
     if (this.clavier.down.isDown) {
       this.player.setVelocityY(260);
       this.player.anims.play("anim_face");
@@ -91,36 +93,5 @@ export default class niveau1 extends Phaser.Scene {
         this.scene.switch("niveau2");
       }
     }
-  }
-  startCountdown() {
-    this.timeLeft = 10 * 60; // 10 minutes en secondes
-    this.timerText = this.add.text(400, 20, "Temps restant: 10:00", {
-      fontSize: "20px", // taille de la police
-      fill: "#fff" // couleur de la police
-    });
-
-    this.time.addEvent({
-      delay: 1000, // delai de 1 seconde
-      callback: () => {
-        this.timeLeft--;
-        let minutes = Math.floor(this.timeLeft / 60); // division entière par 60
-        let seconds = this.timeLeft % 60; // reste de la division par 60
-        this.timerText.setText(`Temps restant: ${minutes}:${seconds.toString().padStart(2, '0')}`); // affichage du temps restant
-
-        if (this.timeLeft <= 0) { // si le temps est écoulé
-          this.killPlayer(); // on tue le joueur
-        }
-      },
-      loop: true // boucle infinie
-    });
-  }
-
-  killPlayer() {
-    this.player.setTint(0xff0000); // on change la couleur du joueur
-    this.player.setVelocity(0, 0); // on arrête le joueur
-    this.player.anims.stop(); // on arrête l'animation du joueur
-    this.time.delayedCall(5000, () => { // on attend 5 secondes
-      this.scene.restart(); // on redémarre la scène
-    });
   }
 }
