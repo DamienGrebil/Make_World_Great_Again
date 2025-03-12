2ea6a99f433f1dcf656da4108964e845e7f68223
etane bigot <etane.bigot@epfedu.fr>
2025-03-12 13:32:55 +0100

    map niveau 3

--- src/js/niveau3.js
+++ src/js/niveau3.js
@@ -1,4 +1,6 @@
 import * as fct from "/src/js/fonctions.js";
+
+var groupe_plateforme_bunker
 export default class niveau3 extends Phaser.Scene {
   // constructeur de la classe
   constructor() {
@@ -6,27 +8,37 @@ export default class niveau3 extends Phaser.Scene {
       key: "niveau3" //  ici on précise le nom de la classe en tant qu'identifiant
     });
   }
-  preload() {}
+  preload() {
+    this.load.image("img_bunker", "src/assets/bunker.png");
+    this.load.image("img_plateforme_bunker", "src/assets/platform_bunker.png");
+    this.load.image("img_plateforme_bunker_mini", "src/assets/platform_bunker_mini.png");
+}
 
   create() {
-    this.add.image(400, 300, "img_ciel");
-    this.groupe_plateformes = this.physics.add.staticGroup();
-    this.groupe_plateformes.create(200, 584, "img_plateforme");
-    this.groupe_plateformes.create(600, 584, "img_plateforme");
+    this.add.image(500, 400, "img_bunker");
+    this.groupe_plateformes_bunker= this.physics.add.staticGroup();
+    this.groupe_plateformes_bunker.create(200, 450, "img_plateforme_bunker");
+    this.groupe_plateformes_bunker.create(600, 450, "img_plateforme_bunker");
+    this.groupe_plateformes_bunker.create(270, 260, "img_plateforme_bunker");
+    this.groupe_plateformes_bunker.create(500, 260, "img_plateforme_bunker");
+    this.groupe_plateformes_bunker.create(50, 334, "img_plateforme_bunker_mini");
+    this.groupe_plateformes_bunker.create(150, 334, "img_plateforme_bunker_mini");
+    this.groupe_plateformes_bunker.create(170, 334, "img_plateforme_bunker_mini");
+    this.groupe_plateformes_bunker.create(800, 310, "img_plateforme_bunker_mini");
     // ajout d'un texte distintcif  du niveau
-    this.add.text(400, 100, "Vous êtes dans le niveau 3", {
+    this.add.text(400, 100, "Vous êtes dans le Bunker", {
       fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
       fontSize: "22pt"
     });
 
-    this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte3");
+    this.porte_retour = this.physics.add.staticSprite(100, 409, "img_porte3");
 
-    this.player = this.physics.add.sprite(100, 450, "img_perso");
+    this.player = this.physics.add.sprite(100, 409, "img_perso");
     this.player.refreshBody();
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
     this.clavier = this.input.keyboard.createCursorKeys();
-    this.physics.add.collider(this.player, this.groupe_plateformes);
+    this.physics.add.collider(this.player, this.groupe_plateformes_bunker);
   }
 
   update() {
