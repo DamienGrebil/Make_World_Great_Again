09dff20cd1ff316558f8d54e9544b561d2cb2bc8
Damien Grebil <damien.grebil@epfedu.fr>
2025-03-12 17:29:29 +0100

    minijeu

--- src/js/minijeu.js
+++ src/js/minijeu.js
@@ -1,6 +1,3 @@
-import * as fct from "/src/js/fonctions.js";
-
-var groupe_plateforme_bunker
 export default class minijeu extends Phaser.Scene {
   // constructeur de la classe
   constructor() {
@@ -9,64 +6,23 @@ export default class minijeu extends Phaser.Scene {
     });
   }
   preload() {
-    this.load.image("img_bunker", "src/assets/bunker.png");
-    this.load.image("img_plateforme_bunker", "src/assets/platform_bunker.png");
-    this.load.image("img_plateforme_bunker_mini", "src/assets/platform_bunker_mini.png");
+    this.load.image("img_num", "src/assets/num.png");
+    this.load.image("bouton_49.3", "src/assets/49.3.png");
 }
 
   create() {
-    this.add.image(500, 400, "img_bunker");
-    this.groupe_plateformes_bunker= this.physics.add.staticGroup();
-    this.groupe_plateformes_bunker.create(200, 450, "img_plateforme_bunker");
-    this.groupe_plateformes_bunker.create(600, 450, "img_plateforme_bunker");
-    this.groupe_plateformes_bunker.create(270, 260, "img_plateforme_bunker");
-    this.groupe_plateformes_bunker.create(500, 260, "img_plateforme_bunker");
-    this.groupe_plateformes_bunker.create(50, 334, "img_plateforme_bunker_mini");
-    this.groupe_plateformes_bunker.create(150, 334, "img_plateforme_bunker_mini");
-    this.groupe_plateformes_bunker.create(170, 334, "img_plateforme_bunker_mini");
-    this.groupe_plateformes_bunker.create(800, 310, "img_plateforme_bunker_mini");
+    let image0 = this.add.image(800, 640, "img_num");
     // ajout d'un texte distintcif  du niveau
-    this.add.text(400, 100, "Ouvrez la porte du bunker Bunker", {
+    this.add.text(400, 100, "Ouvrez la porte du Bunker", {
       fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
       fontSize: "22pt"
     });
 
-    this.porte_retour = this.physics.add.staticSprite(100, 409, "img_cabine");
+let playButton = this.add.image(550, 320, "bouton_49.3").setInteractive();
+    playButton.on("pointerdown", () => {
+      console.log("minijeu : retour vers niv2");
+      this.scene.switch("niveau2");
+    });
 
-    this.player = this.physics.add.sprite(100, 409, "img_perso");
-    this.player.body.setSize(20, 30, 6, 18); // taille du rectangle de collision
-    this.player.body.setOffset(6, 18); // décalage du rectangle de collision
-    this.player.refreshBody();
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
-    this.clavier = this.input.keyboard.createCursorKeys();
-    this.physics.add.collider(this.player, this.groupe_plateformes_bunker);
-  }
-
-  update() {
-    if (this.clavier.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.player.anims.play("anim_tourne_gauche", true);
-    } else if (this.clavier.right.isDown) {
-      this.player.setVelocityX(160);
-      this.player.anims.play("anim_tourne_droite", true);
-    } else {
-      this.player.setVelocityX(0);
-      this.player.anims.play("anim_face");
-    }
-    if (this.clavier.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
-    }
-    if (this.clavier.down.isDown) {
-      this.player.setVelocityY(260);
-      this.player.anims.play("anim_face");
-    }
-
-    if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
-      if (this.physics.overlap(this.player, this.porte_retour)) {
-        console.log("minijeu : retour vers niv2");
-        this.scene.switch("niveau2");
-      }
-    }
   }
 }
